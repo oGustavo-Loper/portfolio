f27d3cf31ee571da304111083017e988b422fcde
oGustavo-Loper <theloper7@gmail.com>
2024-06-03 23:39:44 -0300

    Remove Function FirstAndNames

--- src/pages/utils/FirstAndName.ts
+++
@@ -1,7 +0,0 @@
-export const getFirstAndLastName = (fullName: string) => {
-    const nameParts = fullName.split(' ');
-    if (nameParts.length === 1) {
-      return fullName; // Caso haja apenas um nome
-    }
-    return `${nameParts[0]} ${nameParts[nameParts.length - 1]}`;
-};
