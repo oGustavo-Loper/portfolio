3678303c2ebc694bc10312ff6a619837e0f21e45
oGustavo-Loper <theloper7@gmail.com>
2024-06-04 11:16:29 -0300

    Remove Function FirstAndNames

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>Portfolio Gustavo Loper</title>
+        <meta name="description" content="Portfolio Gustavo Loper" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
